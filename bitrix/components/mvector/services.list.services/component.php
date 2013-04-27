2da7450277e84e29c1de93cc30b05420e9cef444
User-315 <User-315@User-315-PC>
2013-04-27 21:28:19 +0600

    Применение шаблонов компонентов в разделе "Оценка услуг"

--- bitrix/components/mvector/services.list.services/component.php
+++ bitrix/components/mvector/services.list.services/component.php
@@ -3,9 +3,9 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_be
     $arResult = Array();
     $arResult['SERVICES'] = CServices::GetServices();
     $CurLocation = CLocations::GetCurrentLocationID();
-        if($CurLocation > 0) 
-            $arLocation = CLocations::GetLocationParams($CurLocation);
-            $arResult['LOCATION_ALIAS'] = $arLocation[$CurLocation]['LOCATION_ALIAS'];
-
-$this->IncludeComponentTemplate();
+    if($CurLocation > 0) {
+        $arLocation = CLocations::GetLocationParams($CurLocation);
+        $arResult['LOCATION_ALIAS'] = $arLocation[$CurLocation]['LOCATION_ALIAS'];
+        $this->IncludeComponentTemplate();
+    } else echo LOCATION_CHOICE_ERR_MSG;
 ?>
